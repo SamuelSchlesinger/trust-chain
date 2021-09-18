dc59b041f1d01f0a83c34553c0bb10e656fe19b8
Samuel Schlesinger <samuel@simspace.com>
2021-09-18 15:15:18 -0400

    Improved tests

--- test/Test.hs
+++ test/Test.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
+import Data.Binary (encode, decode)
 import Data.TrustChain
 import Cropty
 import System.Exit (exitFailure)
@@ -17,6 +18,7 @@ main = do
   let trustChain0 :: TrustChain [] String = Trustless "Hello"
   trustChain1 <- mkTrustProxy privateKey0 [mkTrustless "Hi", trustChain0]
   trustChain2 <- mkTrustProxy privateKey1 [mkTrustless "Hey", trustChain1]
+  let roundTrip f g x = x == f (g x)
   requires "validTrustChain"
     [ validTrustChain trustChain0
     , validTrustChain trustChain1
@@ -27,3 +29,4 @@ main = do
     , claims trustChain1 == [Claim [privateToPublic privateKey0] "Hi", Claim [privateToPublic privateKey0] "Hello"]
     , claims trustChain2 == [Claim [privateToPublic privateKey1] "Hey", Claim [privateToPublic privateKey1, privateToPublic privateKey0] "Hi", Claim [privateToPublic privateKey1, privateToPublic privateKey0] "Hello"] 
     ]
+  requires "encode/decode" $ map (roundTrip decode encode) [trustChain0, trustChain1, trustChain2]
